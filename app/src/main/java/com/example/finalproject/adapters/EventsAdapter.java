00167ebaf699456ff862ccff58b6571d3794609a
Hannah Yang <hannahyang@fb.com>
2020-07-14 16:52:00 -0700

    Fixed bottom navigation view

---
+++ app/src/main/java/com/example/finalproject/adapters/EventsAdapter.java
@@ -0,0 +1,4 @@
+package com.example.finalproject.adapters;
+
+public class EventsAdapter {
+}
---
+++ app/src/main/java/com/example/finalproject/fragments/EventFragment.java
@@ -0,0 +1,35 @@
+package com.example.finalproject.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.finalproject.R;
+
+import java.util.ArrayList;
+
+public class EventFragment extends Fragment {
+
+    public static final  String TAG = "Event Fragment";
+
+    public EventFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_event, container, false);
+    }
+
+
+}
