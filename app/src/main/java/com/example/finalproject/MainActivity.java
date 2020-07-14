16c0de1bed7818188af7bc94d4f0edaa6b22515f
Hannah Yang <hannahyang@fb.com>
2020-07-14 16:49:32 -0700

    Added host event page

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.finalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.finalproject.fragments.EventFragment;
+import com.example.finalproject.fragments.HostEventFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private  BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // define your fragments here
+//        final Fragment fragment1 = new FirstFragment();
+//        final Fragment fragment2 = new SecondFragment();
+//        final Fragment fragment3 = new ThirdFragment();
+
+        // handle navigation selection
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment fragment;
+                        switch (item.getItemId()) {
+                            case R.id.action_home:
+                                fragment = new EventFragment();
+                                break;
+                            case R.id.action_host:
+                                fragment = new HostEventFragment();
+                                break;
+//                            case R.id.action_profile:
+//                            case R.id.action_user_events:
+
+                            default:
+                                fragment = new EventFragment();
+                                break;
+                        }
+                        fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                        return true;
+                    }
+                });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 }
