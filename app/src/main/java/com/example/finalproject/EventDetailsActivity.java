3d01ad818df3bf72a804e3081d9c8c36d3c3742e
Hannah Yang <hannahyang@fb.com>
2020-07-16 23:01:56 -0700

    Made view for event details

--- app/src/main/java/com/example/finalproject/EventDetailsActivity.java
+++ app/src/main/java/com/example/finalproject/EventDetailsActivity.java
@@ -4,19 +4,62 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.finalproject.models.Event;
+import com.example.finalproject.models.User;
 
 import org.parceler.Parcels;
+import org.w3c.dom.Text;
 
 public class EventDetailsActivity extends AppCompatActivity {
     public static final  String TAG = "EventDetailsActivity";
     private Event event;
+    private static  User user;
+    private TextView tvTitle;
+    private ImageView ivEventPhoto;
+    private ImageView ivProfilePic;
+    private TextView tvName;
+    private TextView tvBio;
+    private TextView tvDate;
+    private TextView tvTime;
+    private TextView tvDescription;
+    private TextView tvLocation;
+    private Button btnRSVP;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
         event = (Event) Parcels.unwrap(getIntent().getParcelableExtra(Event.class.getSimpleName()));
         Log.i(TAG, event.getTitle());
+
+        tvTitle = findViewById(R.id.tvTitle);
+        ivEventPhoto = findViewById(R.id.ivEventPhoto);
+        ivProfilePic = findViewById(R.id.ivProfilePic);
+        tvName = findViewById(R.id.tvName);
+        tvBio = findViewById(R.id.tvBio);
+        tvDate = findViewById(R.id.tvDate);
+        tvTime = findViewById(R.id.tvTime);
+        tvDescription = findViewById(R.id.tvDescription);
+        tvLocation = findViewById(R.id.tvLocation);
+
+        tvTitle.setText(event.getTitle());
+        if(event.getImageUrl()!=null){
+            Glide.with(EventDetailsActivity.this).load(event.getImageUrl()).into(ivEventPhoto);
+        }
+        //tvName.setText(user.getScreenName());
+        //tvName.setText(user.getBio());
+        // TODO: Set profile pic, name + bio
+        tvDate.setText(event.getDate());
+        tvTime.setText(event.getTime());
+        tvDescription.setText(event.getDescription());
+        tvLocation.setText(event.getAddress());
     }
+    public static void setUser(User user1){
+        user = user1;
+    }
+
 }
