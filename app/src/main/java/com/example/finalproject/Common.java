36ae3dac14a40e82177a93f115d0b607a2d09d29
Hannah Yang <hannahyang@fb.com>
2020-07-20 16:02:27 -0700

    Can view image of map in event details activity

--- app/src/main/java/com/example/finalproject/Common.java
+++ app/src/main/java/com/example/finalproject/Common.java
@@ -5,4 +5,6 @@ public class Common {
     public static final String EVENT_ATTENDING_KEY = "attending";
     public static final String EVENT_HOSTING_KEY = "hosting";
     public static final String MAP_SEARCH_URL_KEY = "https://www.google.com/maps/search/?api=1&query=";
+    public static final String MAP_STATIC_URL_KEY = "https://maps.googleapis.com/maps/api/staticmap?center=";
+
 }
--- app/src/main/java/com/example/finalproject/EventDetailsActivity.java
+++ app/src/main/java/com/example/finalproject/EventDetailsActivity.java
@@ -1,13 +1,23 @@
 package com.example.finalproject;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
 
+import android.content.ContentProviderOperation;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
+import android.view.textclassifier.TextLinks;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,9 +29,20 @@ import com.google.android.material.floatingactionbutton.ExtendedFloatingActionBu
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 import org.parceler.Parcels;
 import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class EventDetailsActivity extends AppCompatActivity {
     public static final  String TAG = "EventDetailsActivity";
@@ -78,6 +99,7 @@ public class EventDetailsActivity extends AppCompatActivity {
                 DatabaseClient.rsvpUser(event, EventDetailsActivity.this);
             }
         });
+        setGoogleMapThumbnail(event.getAddress());
     }
 
     // Q: is it worth it to separate this out into a method?
@@ -103,6 +125,36 @@ public class EventDetailsActivity extends AppCompatActivity {
     private void launchGoogleMaps(){
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(Common.MAP_SEARCH_URL_KEY + convertAddressToSearchQuery(event.getAddress())));
         startActivity(intent);
+    }
+
+    public void setGoogleMapThumbnail(String address){
+        Log.i(TAG, getString(R.string.google_api_key));
+        String url = Common.MAP_STATIC_URL_KEY+ convertAddressToSearchQuery(address) + "zoom=14&size=400x300&key=" + getString(R.string.api_key);
+        Log.i(TAG, url);
+        final Request request = new Request.Builder().url(url).build();
+        OkHttpClient okHttpClient = new OkHttpClient();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                if (response.isSuccessful()){
+                    Log.i(TAG, response.toString());
+                    final Bitmap bitmap = BitmapFactory.decodeStream(response.body().byteStream());
+                    new Handler(Looper.getMainLooper()).post(new Runnable() {
+                        @Override
+                        public void run() {
+                            ivMap.setImageBitmap(bitmap);
+                        }
+                    });
+                }else {
+                    //Handle the error
+                }
+            }
+        });
     }
 
     // Q: Should this go in it's own file?
