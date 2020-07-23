f4117ee201e632b053928790db637a8e15b6be1c
Hannah Yang <hannahyang@fb.com>
2020-07-23 11:07:05 -0700

    Now store dates in firebase as YYYY/MM/DD

--- app/src/main/java/com/example/finalproject/fragments/HostEventFragment.java
+++ app/src/main/java/com/example/finalproject/fragments/HostEventFragment.java
@@ -1,28 +1,13 @@
 package com.example.finalproject.fragments;
 
 import android.app.DatePickerDialog;
-import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.AssetFileDescriptor;
-import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.media.ExifInterface;
-import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
-import androidx.fragment.app.Fragment;
-
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,39 +18,30 @@ import android.widget.ImageView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+
 import com.example.finalproject.DatabaseClient;
 import com.example.finalproject.ImageFormatter;
-import com.example.finalproject.MainActivity;
 import com.example.finalproject.R;
-import com.example.finalproject.models.Event;
-import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
 import java.util.Calendar;
-import java.util.List;
-import java.util.UUID;
 
 import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
+import static com.example.finalproject.TimeAndDateFormatter.formatDateForStorage;
+import static com.example.finalproject.TimeAndDateFormatter.formatDateForView;
+import static com.example.finalproject.TimeAndDateFormatter.formatTime;
 
 
 public class HostEventFragment extends Fragment {
 
-    public static final  String TAG = "Event Fragment";
+    public static final String TAG = "Event Fragment";
     private static final int DEFAULT_MIN_WIDTH_QUALITY = 400;        // min pixels
     //private static Uri downloadUri;
 
@@ -121,7 +97,7 @@ public class HostEventFragment extends Fragment {
                         new DatePickerDialog.OnDateSetListener() {
                             @Override
                             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                                etDate.setText( (monthOfYear + 1) + "/"  +dayOfMonth +"/" + year);
+                                etDate.setText(formatDateForView(year, monthOfYear, dayOfMonth));
                             }
                         }, year, month, day);
                 picker.show();
@@ -147,9 +123,9 @@ public class HostEventFragment extends Fragment {
                 String title = etTitle.getText().toString();
                 String description = etDescription.getText().toString();
                 String address = etAddress.getText().toString();
-                String date = etDate.getText().toString();
+                String date = formatDateForStorage(etDate.getText().toString());
                 String time = etTime.getText().toString();
-                if (description.isEmpty()){
+                if (description.isEmpty()) {
                     Toast.makeText(getContext(), "Description cannot be empty", Toast.LENGTH_SHORT).show();
                     return;
                 }
@@ -167,27 +143,11 @@ public class HostEventFragment extends Fragment {
             }
         });
     }
-    // Takes in hours/24 and minutes/60 and formats it as 1:00 PM form
-    public String formatTime(int hour, int min) {
-        String format = " AM";
-        String minutes = ""+ min;
-        if (hour>=12) {
-            format = " PM";
-        }
-        if (hour == 0) {
-            hour += 12;
-        } else if (hour > 12) {
-            hour -=12;
-        }
-        if (min<10){
-            minutes = "0"+ minutes;
-        }
-        return ""+ hour + ":" + minutes + format;
-    }
+
 
     //Pops up a dialog to ask the user whether they would like to get a photo from camera or gallery
     private void selectImage(Context context) {
-        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
+        final CharSequence[] options = {"Take Photo", "Choose from Gallery", "Cancel"};
 
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle("Choose a photo for your event");
@@ -203,7 +163,7 @@ public class HostEventFragment extends Fragment {
 
                 } else if (options[item].equals("Choose from Gallery")) {
                     Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    startActivityForResult(pickPhoto , 1);
+                    startActivityForResult(pickPhoto, 1);
 
                 } else if (options[item].equals("Cancel")) {
                     dialog.dismiss();
@@ -216,39 +176,32 @@ public class HostEventFragment extends Fragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if(resultCode != RESULT_CANCELED) {
+        if (resultCode != RESULT_CANCELED) {
             Bitmap bm = null;
             switch (requestCode) {
                 case 0:  // If photo from Camera
                     if (resultCode == RESULT_OK && data != null) {
                         bm = (Bitmap) data.getExtras().get("data");
-                        ivPhoto.setImageBitmap(bm);
-                        imageToUpload = ImageFormatter.getImageUri(getContext(), bm);
                     }
                     break;
                 case 1:     // If photo from Gallery
                     if (resultCode == RESULT_OK && data != null) {
-                        Uri selectedImage =  data.getData();
+                        Uri selectedImage = data.getData();
                         bm = ImageFormatter.getImageResized(getContext(), selectedImage);
                         int rotation = ImageFormatter.getRotation(getContext(), selectedImage, false);
                         bm = ImageFormatter.rotate(bm, rotation);
-                        ivPhoto.setImageBitmap(bm);
-                        //uploadImage(selectedImage);
                     }
                     break;
             }
+            ivPhoto.setImageBitmap(bm);
             imageToUpload = ImageFormatter.getImageUri(getContext(), bm);
-            //uploadImage(imageToUpload);
             DatabaseClient.uploadImage(new OnCompleteListener<Uri>() {
                 @Override
                 public void onComplete(@NonNull Task<Uri> task) {
-                    downloadUri =  task.getResult();
+                    downloadUri = task.getResult();
                 }
             }, imageToUpload, getContext());
         }
     }
-//    public static void setDownloadUri(Uri uri){
-//        downloadUri = uri;
-//    }
 }
 
